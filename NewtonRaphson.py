6dc04abf06152deb65f91789a9325cb101884f44
Muhammad Sujak MK <anack.magja@gmail.com>
2023-07-08 19:00:54 +0900

    NewtonRaphson.py has created!

--- NewtonRaphson.py
+++ NewtonRaphson.py
@@ -1,6 +1,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+def main():
+    xn = 5
+    err = 0.01
+    maxit = 100
+    NewtonRaphson(xn,err,maxit)
+
 
 def NewtonRaphson(xn,err=0.0001,maxit=50):
     t=0 
@@ -13,11 +19,9 @@ def NewtonRaphson(xn,err=0.0001,maxit=50):
             print(f'root is= {xn}')
             break
 
-
 def func(x):
     f = x**2-2*x
     df = 2*x-x
     return f,df 
 
-
-NewtonRaphson(2)
+main()
